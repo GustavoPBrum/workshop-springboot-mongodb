43e274265b258d497746cbea3532962ebaedd445
GustavoPBrum <brumtavo@gmail.com>
2025-05-21 13:36:12 +0100

    Atualização de usuário com PUT

--- src/main/java/com/estudandojava/workshopmongo/resources/UserResource.java
+++ src/main/java/com/estudandojava/workshopmongo/resources/UserResource.java
@@ -6,13 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudandojava.workshopmongo.domain.User;
@@ -58,4 +52,13 @@ public class UserResource {
 		
 		return ResponseEntity.noContent().build();  // Codigo para resposta vazia 204
 	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDto(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
+	}	
 }
