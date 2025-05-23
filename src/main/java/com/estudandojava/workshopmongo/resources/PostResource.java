e50aa6150b68b8d21c5382bd83b6b73ee1b363d2
GustavoPBrum <brumtavo@gmail.com>
2025-05-23 11:58:39 +0100

    Consulta simples com query methods

--- src/main/java/com/estudandojava/workshopmongo/resources/PostResource.java
+++ src/main/java/com/estudandojava/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudandojava.workshopmongo.domain.Post;
+import com.estudandojava.workshopmongo.resources.util.URL;
 import com.estudandojava.workshopmongo.services.PostService;
 
 @RestController
@@ -25,10 +27,17 @@ public class PostResource {
 	    return ResponseEntity.ok().body(obj);
 	}
 	
-
 	@GetMapping
 	public ResponseEntity<List<Post>> findAll() {
 	    List<Post> list = service.findAll();
+	    return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/titlesearch")  // Se o 'text' nao for informado, sera inserido uma String vazia
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+	    text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+
 	    return ResponseEntity.ok().body(list);
 	}
 }
