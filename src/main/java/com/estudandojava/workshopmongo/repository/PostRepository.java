184575b7c37fe6f2292052d81ec7672292395e5e
GustavoPBrum <brumtavo@gmail.com>
2025-05-23 13:47:16 +0100

    Consulta com vários critérios

--- src/main/java/com/estudandojava/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/estudandojava/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.estudandojava.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,10 +11,16 @@ import com.estudandojava.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-	
+
 	// '?0' quer dizer o **primeiro** parametro, se fosse o segundo seria '?1' e assim por diante
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
-	
-	List<Post> findByTitleContainingIgnoreCase(String text);  //Query methods
+
+	List<Post> findByTitleContainingIgnoreCase(String text); // Query methods
+
+	@Query("{ $and: [ { date: {$gte: ?1}}, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } },"
+			+ " { 'body': { $regex: ?0, $options: 'i' } }, "
+			+ "{ 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }") // Para buscar o texto dentro de cada
+																			// comentario
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/estudandojava/workshopmongo/resources/util/URL.java
+++ src/main/java/com/estudandojava/workshopmongo/resources/util/URL.java
@@ -2,14 +2,29 @@ package com.estudandojava.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 	
 	public static String decodeParam(String text) {
 		try {
-			return URLDecoder.decode(text, "UTF-8");
+			return URLDecoder.decode(text, "ASCII");
 		} catch (UnsupportedEncodingException e) {
-			return "";
+			return "numdeu";
+		}
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			return defaultValue;
 		}
 	}
 }
